de27c3c89fab2534e678eb1f92e58642e307095d
Eitan Suez <eitan.suez@gmail.com>
2017-06-12 14:03:58 -0500

    corrected erroneously-named method

--- server-regions/src/main/java/io/pivotal/bookshop/buslogic/BookLoader.java
+++ server-regions/src/main/java/io/pivotal/bookshop/buslogic/BookLoader.java
@@ -15,18 +15,18 @@ public class BookLoader {
       Region<Long, Book> bookRegion = cache.getRegion("Book");
 
       Book book = new Book(123, "Run on sentences and drivel on all things mundane", (float) 34.99, 2011, "Daisy Mae West", "A Treatise of Treatises");
-      addCustomer(bookRegion, 123L, book);
+      addBook(bookRegion, 123L, book);
 
       Book book2 = new Book(456, "A book about a dog", (float) 11.99, 1971, "Clarence Meeks", "Clifford the Big Red Dog");
-      addCustomer(bookRegion, 456L, book2);
+      addBook(bookRegion, 456L, book2);
 
       Book book3 = new Book(789, "Theoretical information about the structure of Operating Systems", (float) 59.99, 2011, "Jim Heavisides", "Operating Systems: An Introduction");
-      addCustomer(bookRegion, 789L, book3);
+      addBook(bookRegion, 789L, book3);
 
     }
   }
 
-  private static void addCustomer(Region<Long, Book> bookRegion, long key, Book book) {
+  private static void addBook(Region<Long, Book> bookRegion, long key, Book book) {
     bookRegion.put(key, book);
     log.info("Inserted a book: "+book);
   }
