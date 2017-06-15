1ed533aed30d7855a2f60f2e8a6cff0409e9c3a2
Eitan Suez <eitan.suez@gmail.com>
2017-06-15 16:09:19 -0500

    base url for rest api appears to have changed to geode/v1

--- gemfire-rest/src/test/java/io/pivotal/bookshop/tests/RestClientTest.java
+++ gemfire-rest/src/test/java/io/pivotal/bookshop/tests/RestClientTest.java
@@ -10,24 +10,24 @@ import io.pivotal.bookshop.domain.BookOrder;
 
 
 public class RestClientTest {
-	// TODO-07: Change the port to 8081. Next, show the view: TCP/IP Monitor and configure using the
-	//          detailed lab instructions. Re-run the test and observe the request & response.
-	private static final String BASE_URL = "http://localhost:8080/gemfire-api/v1";
+  // TODO-07: Change the port to 8081. Next, show the view: TCP/IP Monitor and configure using the
+  //          detailed lab instructions. Re-run the test and observe the request & response.
+  private static final String BASE_URL = "http://localhost:8080/geode/v1";
 
-	private RestTemplate restTemplate = new RestTemplate();
+  private RestTemplate restTemplate = new RestTemplate();
 
-	@Test
-	// TODO-06: Run this test. Did it run properly? If not, why not?
-	// TODO-09: Re-run this test after properly specifying formatting on the BookOrder class
-	public void testGetBookOrder() {
-		// TODO-04: Define the URL that will be used to fetch the entry from the BookOrder region.
-		//          Use URI template syntax to allow passing the entry key as a parameter.
-		String url = "";
+  @Test
+  // TODO-06: Run this test. Did it run properly? If not, why not?
+  // TODO-09: Re-run this test after properly specifying formatting on the BookOrder class
+  public void testGetBookOrder() {
+    // TODO-04: Define the URL that will be used to fetch the entry from the BookOrder region.
+    //          Use URI template syntax to allow passing the entry key as a parameter.
+    String url = "";
 
-		// TODO-05: Issue a call to the RestTemplate to fetch an entry having the key 17699 and assigning to the 'order' variable.
-		BookOrder order = null;
-		assertNotNull(order);
-		assertEquals(17699L, order.getOrderNumber());
-	}
+    // TODO-05: Issue a call to the RestTemplate to fetch an entry having the key 17699 and assigning to the 'order' variable.
+    BookOrder order = null;
+    assertNotNull(order);
+    assertEquals(17699L, order.getOrderNumber());
+  }
 
 }
