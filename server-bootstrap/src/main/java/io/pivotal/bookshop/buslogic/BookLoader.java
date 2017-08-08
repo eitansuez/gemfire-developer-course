781e5076c1edb2a93da91d474c99ed9020fb38d0
Eitan Suez <eitan.suez@gmail.com>
2017-08-08 10:46:23 -0600

    reverting use of builder for server-bootstrap for now

--- server-bootstrap/src/main/java/io/pivotal/bookshop/buslogic/BookLoader.java
+++ server-bootstrap/src/main/java/io/pivotal/bookshop/buslogic/BookLoader.java
@@ -3,8 +3,6 @@ package io.pivotal.bookshop.buslogic;
 import io.pivotal.bookshop.domain.Book;
 import lombok.extern.log4j.Log4j2;
 import org.apache.geode.cache.Region;
-import org.apache.geode.cache.client.ClientCache;
-import org.apache.geode.cache.client.ClientCacheFactory;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,34 +10,33 @@ import org.springframework.stereotype.Component;
 public class BookLoader {
 
   public void populateBooks(Region<Long, Book> bookRegion) {
-    Book book = Book.builder()
-        .itemNumber(123)
-        .description("Run on sentences and drivel on all things mundane")
-        .retailCost(34.99f)
-        .yearPublished(2011)
-        .author("Daisy Mae West")
-        .title("A Treatise of Treatises")
-        .build();
+    Book book = new Book(123,
+        "Run on sentences and drivel on all things mundane",
+        34.99f,
+        2011,
+        "Daisy Mae West",
+        "A Treatise of Treatises"
+        );
     addBook(book, bookRegion);
 
-    Book book2 = Book.builder()
-        .itemNumber(456)
-        .description("A book about a dog")
-        .retailCost(11.99f)
-        .yearPublished(1971)
-        .author("Clarence Meeks")
-        .title("Clifford the Big Red Dog")
-        .build();
+    Book book2 = new Book(
+        456,
+        "A book about a dog",
+        11.99f,
+        1971,
+        "Clarence Meeks",
+        "Clifford the Big Red Dog"
+        );
     addBook(book2, bookRegion);
 
-    Book book3 = Book.builder()
-        .itemNumber(789)
-        .description("Theoretical information about the structure of Operating Systems")
-        .retailCost(59.99f)
-        .yearPublished(2011)
-        .author("Jim Heavisides")
-        .title("Operating Systems: An Introduction")
-        .build();
+    Book book3 = new Book(
+        789,
+        "Theoretical information about the structure of Operating Systems",
+        59.99f,
+        2011,
+        "Jim Heavisides",
+        "Operating Systems: An Introduction"
+        );
     addBook(book3, bookRegion);
   }
 
