b6bc3958ffda608cae7399d0c9b753020c90d8e0
Eitan Suez <eitan.suez@gmail.com>
2017-09-19 16:52:51 -0400

    cleaning up test class for server events lab

--- server-events/src/test/java/io/pivotal/bookshop/tests/ServerEventsTests.java
+++ server-events/src/test/java/io/pivotal/bookshop/tests/ServerEventsTests.java
@@ -17,13 +17,20 @@ public class ServerEventsTests {
 
   private ClientCache clientCache;
   private Region<String, Book> bookRegion;
+  private Book book = new Book(124,
+      "A spy fiction thriller about a retrograde amnesiac " +
+          "who must discover who he is ",
+      34.99f,
+      2011,
+      "Robert Ludlum",
+      "Bourne Identity");
 
   @Before
   public void setup() {
     clientCache = new ClientCacheFactory().create();
     bookRegion = clientCache.getRegion("Book");
 
-    // Ensure that no entries exist before test runs:
+    // Ensure no entries exist before test runs:
     bookRegion.removeAll(bookRegion.keySetOnServer());
   }
 
@@ -32,13 +39,6 @@ public class ServerEventsTests {
     clientCache.close();
   }
 
-  /**
-   * Asserts that when a key ('Key00') is requested that a new one is created by the cache loader.
-   * First asserts that there is no entry with Key00, then performs a get operation, which should
-   * trigger the CacheLoader behavior. Then, asserts that a non-null value was returned AND that
-   * there is now a key = 'Key00' on the server AND the author and item number are as expected.
-   * Note: These last two tests are probably unnecessary but were at least interesting to validate.
-   */
   @Test
   public void shouldLoadEntryOnCacheMiss() {
     String key = "Key00";
@@ -51,23 +51,16 @@ public class ServerEventsTests {
     assertThat(newBook.getItemNumber()).isEqualTo(123);
   }
 
-  /**
-   * Asserts that when inserting a valid entry that no errors occur
-   */
   @Test
   public void shouldAllowEntryWrite() {
-    bookRegion.put("Key01", new Book(124, "A spy fiction thriller about a retrograde amnesiac who must discover who he is ", (float) 34.99, 2011, "Robert Ludlum", "Bourne Identity"));
+    bookRegion.put("Key01", book);
   }
 
-  /**
-   * Asserts that when attempting to insert two entries with the same itemNumber, the second one fails with an expected
-   * CacheWriterException.
-   */
   @Test()
   public void shouldVetoEntryWithDuplicateItemNumber() {
-    bookRegion.put("Key01", new Book(124, "A spy fiction thriller about a retrograde amnesiac who must discover who he is ", (float) 34.99, 2011, "Robert Ludlum", "Bourne Identity"));
+    bookRegion.put("Key01", book);
     try {
-      bookRegion.put("Key02", new Book(124, "A spy fiction thriller about a retrograde amnesiac who must discover who he is ", (float) 34.99, 2011, "Robert Ludlum", "Bourne Identity"));
+      bookRegion.put("Key02", book);
       fail("Failed to throw an exception");
     } catch (Exception e) {
       assert e.getCause() instanceof CacheWriterException;
