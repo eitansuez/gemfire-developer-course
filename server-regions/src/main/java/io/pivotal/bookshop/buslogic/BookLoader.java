53d46c7c9c5e92e9296d3e9414e43267fce5a0d7
Eitan Suez <eitan.suez@gmail.com>
2017-09-23 10:58:16 -0500

    server regions, removed fixtures duplication

--- server-regions/src/main/java/io/pivotal/bookshop/buslogic/BookLoader.java
+++ server-regions/src/main/java/io/pivotal/bookshop/buslogic/BookLoader.java
@@ -1,6 +1,7 @@
 package io.pivotal.bookshop.buslogic;
 
 import io.pivotal.bookshop.domain.Book;
+import io.pivotal.bookshop.fixtures.Fixtures;
 import lombok.extern.log4j.Log4j2;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
@@ -14,43 +15,11 @@ public class BookLoader {
     try (ClientCache cache = new ClientCacheFactory().create()) {
       Region<Long, Book> bookRegion = cache.getRegion("Book");
 
-      Book book = Book.builder()
-          .itemNumber(123)
-          .description("Run on sentences and drivel on all things mundane")
-          .retailCost(34.99f)
-          .yearPublished(2011)
-          .author("Daisy Mae West")
-          .title("A Treatise of Treatises")
-          .build();
-      addBook(bookRegion, book);
-
-      Book book2 = Book.builder()
-          .itemNumber(456)
-          .description("A book about a dog")
-          .retailCost(11.99f)
-          .yearPublished(1971)
-          .author("Clarence Meeks")
-          .title("Clifford the Big Red Dog")
-          .build();
-      addBook(bookRegion, book2);
-
-      Book book3 = Book.builder()
-          .itemNumber(789)
-          .description("Theoretical information about the structure of Operating Systems")
-          .retailCost(59.99f)
-          .yearPublished(2011)
-          .author("Jim Heavisides")
-          .title("Operating Systems: An Introduction")
-          .build();
-      addBook(bookRegion, book3);
+      Fixtures.someBooks().forEach(book -> {
+        bookRegion.put(book.getItemNumber(), book);
+        log.info("Inserted book: " + book);
+      });
     }
   }
-
-  private static void addBook(Region<Long, Book> bookRegion, Book book) {
-    Long key = book.getItemNumber();
-    bookRegion.put(key, book);
-    log.info("Inserted a book: "+book);
-  }
-
 
 }
--- server-regions/src/main/java/io/pivotal/bookshop/buslogic/CustomerLoader.java
+++ server-regions/src/main/java/io/pivotal/bookshop/buslogic/CustomerLoader.java
@@ -2,6 +2,7 @@ package io.pivotal.bookshop.buslogic;
 
 import io.pivotal.bookshop.domain.Address;
 import io.pivotal.bookshop.domain.Customer;
+import io.pivotal.bookshop.fixtures.Fixtures;
 import lombok.extern.log4j.Log4j2;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
@@ -15,29 +16,12 @@ public class CustomerLoader {
     try (ClientCache cache = new ClientCacheFactory().create()) {
       Region<Long, Customer> customerRegion = cache.getRegion("Customer");
 
-      Customer cust1 = Customer.builder().customerNumber(5598)
-          .firstName("Kari").lastName("Powell")
-          .bookOrder(17699L).bookOrder(18009L).bookOrder(18049L)
-          .address(Address.builder().postalCode("44444").build()).build();
-      addCustomer(customerRegion, cust1);
+      Fixtures.someCustomers().forEach(customer -> {
+        customerRegion.put(customer.getCustomerNumber(), customer);
+        log.info("Inserted a customer: " + customer);
+      });
 
-      Customer cust2 = Customer.builder().customerNumber(5543)
-          .firstName("Lula").lastName("Wax")
-          .bookOrder(17699L)
-          .address(Address.builder().postalCode("12345").build()).build();
-      addCustomer(customerRegion, cust2);
-
-      Customer cust3 = Customer.builder().customerNumber(6024)
-          .firstName("Trenton").lastName("Garcia")
-          .address(Address.builder().postalCode("88888").build()).build();
-      addCustomer(customerRegion, cust3);
     }
-  }
-
-  private static void addCustomer(Region<Long, Customer> customerRegion, Customer customer) {
-    long key = customer.getCustomerNumber();
-    customerRegion.put(key, customer);
-    log.info("Inserted a customer: " + customer);
   }
 
 }
