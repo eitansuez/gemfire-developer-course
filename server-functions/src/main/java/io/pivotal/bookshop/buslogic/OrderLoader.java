a9a53bfc6d069f084d4b5faae066542c87a9390f
Eitan Suez <eitan.suez@gmail.com>
2017-09-23 09:07:47 -0500

    removing unused domain types in server-functions module

--- server-functions/src/main/java/io/pivotal/bookshop/buslogic/OrderLoader.java
+++ server-functions/src/main/java/io/pivotal/bookshop/buslogic/OrderLoader.java
@@ -12,11 +12,9 @@ import java.util.Date;
 public class OrderLoader {
 
   public static void main(String[] args) {
-    ClientCache cache = new ClientCacheFactory().create();
-
-    populateBookOrders(cache);
-
-    cache.close();
+    try (ClientCache cache = new ClientCacheFactory().create()) {
+      populateBookOrders(cache);
+    }
   }
 
   private static void populateBookOrders(ClientCache cache) {
@@ -24,16 +22,18 @@ public class OrderLoader {
     // Order for Kari Powell for book: A Treatise of Treatises
     BookOrder order1 = BookOrder.builder().orderNumber(17699).orderDate(new Date())
         .shippingCost(5.99f).shipDate(new Date())
-        .customerNumber(5598).totalPrice(40.98f).build();
-    order1.addOrderItem(BookOrderItem.builder().orderLine(1).itemNumber(123).build());
+        .orderItem(BookOrderItem.builder().orderLine(1).itemNumber(123).build())
+        .customerNumber(5598).totalPrice(40.98f)
+      .build();
     orders.put(17699L, order1);
 
     // Order for Lula Wax   book: A Treatise of Treatises & Clifford the Big Red Dog
     BookOrder order2 = BookOrder.builder().orderNumber(17700).orderDate(new Date())
         .shippingCost(5.99f).shipDate(new Date())
-        .customerNumber(5543).totalPrice(52.97f).build();
-    order2.addOrderItem(BookOrderItem.builder().orderLine(1).itemNumber(123).build());
-    order2.addOrderItem(BookOrderItem.builder().orderLine(2).itemNumber(456).build());
+        .orderItem(BookOrderItem.builder().orderLine(1).itemNumber(123).build())
+        .orderItem(BookOrderItem.builder().orderLine(2).itemNumber(456).build())
+        .customerNumber(5543).totalPrice(52.97f)
+      .build();
     orders.put(17700L, order2);
   }
 
